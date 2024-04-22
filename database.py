8941ee4f6d75cbd144b41d5f616ec09e69c753af
ICT_LAPTOP02S <christiandizon0527@gmail.com>
2024-04-22 08:31:35 +0800

    Designing...

--- database.py
+++ database.py
@@ -4,12 +4,12 @@ conn = odbccon.connect( "Driver={ODBC Driver 13 for SQL Server};"
                         "Server=ICT_LAPTOP02S;"
                         "Database=eDevInventoy;"
                         "Trusted_Connection=yes;")
-'''
+
 cursor = conn.cursor()
 cursor.execute('Select * from Computers')
 for row in cursor:
     print(row)
-'''
+
 '''
 def main_page_db():
 
