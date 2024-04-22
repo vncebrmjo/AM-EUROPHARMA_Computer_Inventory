c3c0b00d9a644c1cfc418c15fbd6198af77195a8
ICT_LAPTOP02S <christiandizon0527@gmail.com>
2024-04-22 15:46:13 +0800

    Revert "database.py improvement, main.py function improvement"
    This reverts commit 72360a39aee4ff8d388cea987a3430310006d483.

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
-from db_utils import main_page_db
+import pyodbc as odbccon
 
 app = Flask(__name__)
 
+conn = odbccon.connect( "Driver={ODBC Driver 13 for SQL Server};"
+                        "Server=ICT_LAPTOP02S;"
+                        "Database=eDevInventoy;"
+                        "Trusted_Connection=yes;")
 @app.route('/')
-def main_page():
-    data = main_page_db()
-    return render_template('main_page.html', data=data)
-
+def main_page_form():
+#    cursor = conn.cursor()
+#    cursor.execute('Select * from Computers')
+#    for row in cursor:
+#        print(row)
+    return render_template('main_page.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
