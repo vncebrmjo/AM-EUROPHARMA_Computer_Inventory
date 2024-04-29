0e64869a7a2f3efe47980d66d4b05b2167b26d03
ICT_LAPTOP02S <christiandizon0527@gmail.com>
2024-04-29 16:00:51 +0800

    header update, main.py modifications, db utils, and details.html addition

--- db_utils.py
+++ db_utils.py
@@ -1,16 +1,28 @@
 import pyodbc as odbcconn
-from flask import request
+
 
 connect = odbcconn.connect("Driver={ODBC Driver 13 for SQL Server};"
                         "Server=ICT_LAPTOP02S;"
                         "Database=eDevInventoy;"
                         "Trusted_Connection=yes;")
 
+def main_page_db(department):
+    cursor = connect.cursor()
+    if department:
+        cursor.execute("SELECT * FROM Computers WHERE Department = ?",
+                       ( department,))
+    else:
+        cursor.execute("SELECT * FROM Computers")
 
+    department = cursor.fetchall()
+    cursor.close()
+    return department
 
-def main_page_db():
+def details_db():
     cursor = connect.cursor()
     cursor.execute("SELECT * FROM Computers")
-    tbl_computers = cursor.fetchall()
+
+    details = cursor.fetchall()
     cursor.close()
-    return tbl_computers
+    return details
+
--- main.py
+++ main.py
@@ -1,20 +1,36 @@
-from flask import Flask, render_template,request
-from db_utils import main_page_db
+from flask import Flask, render_template, request, url_for
+from db_utils import main_page_db, details_db
+import pyodbc as odbcconn
 
 app = Flask(__name__)
 
 Total_Pages = 5
 
+connect = odbcconn.connect("Driver={ODBC Driver 13 for SQL Server};"
+                        "Server=ICT_LAPTOP02S;"
+                        "Database=eDevInventoy;"
+                        "Trusted_Connection=yes;")
 
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def main_page():
-    page = request.args.get('page', 1, type=int)
-    page = max(page, 1)
-    offset = (page - 1) * Total_Pages
-    data = main_page_db()
+    if request.method == 'POST':
+        selected_department = request.form.get('department')
+    else:
+        selected_department = None
+
+    data = main_page_db(selected_department)
+
     return render_template('main_page.html', data=data)
 
+@app.route('/details')
+def details():
+    fetch = details_db()
+    return render_template('details.html', fetch=fetch)
+
+@app.route('/Add_Inventory')
+def Add_Inventory():
+
+    return render_template('Add_Inventory.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
