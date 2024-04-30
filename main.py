6b8273de515b5d3673c4f2d047b2e8e40a0067ac
ICT_LAPTOP02S <christiandizon0527@gmail.com>
2024-04-30 16:07:32 +0800

    main_page design, details.html design, restructure database in main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, url_for
-from db_utils import main_page_db, details_db
 import pyodbc as odbcconn
 
 app = Flask(__name__)
@@ -13,19 +12,49 @@ connect = odbcconn.connect("Driver={ODBC Driver 13 for SQL Server};"
 
 @app.route('/', methods=['POST', 'GET'])
 def main_page():
+    cursor = connect.cursor()
+    department = request.form.get('department')
+
     if request.method == 'POST':
-        selected_department = request.form.get('department')
+        cursor.execute("SELECT * FROM Computers WHERE Department = ?",
+                       (department,))
+
     else:
-        selected_department = None
+        cursor.execute("SELECT * FROM Computers")
 
-    data = main_page_db(selected_department)
+    data = cursor.fetchall()
+    cursor.close()
 
-    return render_template('main_page.html', data=data)
+    return render_template('main_page.html', department=department, data=data)
 
-@app.route('/details')
+@app.route('/details', methods=['GET', 'POST'])
 def details():
-    fetch = details_db()
-    return render_template('details.html', fetch=fetch)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        user = request.args.get('user')
+        computer_name = request.args.get('computer_name')
+        processor = request.args.get('processor')
+        motherboard = request.args.get('motherboard')
+        power_supply = request.args.get('power_supply')
+        ram = request.args.get('ram')
+        storage = request.args.get('storage')
+        os = request.args.get('os')
+        eset = request.args.get('eset')
+        msoffice = request.args.get('msoffice')
+        asset_tag = request.args.get('asset_tag')
+        computer_tag = request.args.get('computer_tag')
+        network_tag = request.args.get('network_tag')
+        ups = request.args.get('ups')
+        serial_number = request.args.get('serial_number')
+
+        cursor = connect.cursor()
+        cursor.execute("SELECT * FROM Computers WHERE ID = ?",
+                       (id,))
+        details = cursor.fetchone()
+
+    return render_template('details.html', id=id, processor=processor, motherboard=motherboard, power_supply=power_supply,
+                           ram=ram, storage=storage, os=os, eset=eset, msoffice=msoffice, asset_tag=asset_tag, computer_tag=computer_tag,
+                           network_tag=network_tag, ups=ups, serial_number=serial_number, details=details, user=user, computer_name=computer_name)
 
 @app.route('/Add_Inventory')
 def Add_Inventory():
