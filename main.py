098af0f7152d767b377b322d9dc8ed17944db347
ICT_LAPTOP02S <christiandizon0527@gmail.com>
2024-05-21 14:58:32 +0800

    mainpage optimization, report logs optimization, main.py insert data

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 import pyodbc as odbcconn
 from math import ceil
 
 app = Flask(__name__)
+
+
 
 
 cursor = connect.cursor()
@@ -113,21 +115,49 @@ def add_inventory2():
     #         and 'network_tag' in request.form and 'btn_ups' in request.form and 'btn_opstat' in request.form and 'ps_1' + ' ' + 'ps_2' in request.form:
     if request.method == "POST":
         department = request.form.get('department')
-        #user = request.form['user']
+        user = request.form['user']
         computer_name = request.form['computer_name']
         ip = request.form['ip']
-        processor = request.form.get('processor_1 ' + ' ' + 'processor_2' + ' ' + ' processor_3')
-        mobo = request.form.get('mobo_1' + ' ' + 'mobo_2')
-        ps = request.form.get('ps_1' + ' ' + 'ps_2')
-        ram = request.form.get('ram_1' + ' ' + 'ram_2' + ' ' + 'ram_3')
+
+        processor_1 = request.form.get('processor_1')
+        processor_2 = request.form.get('processor_2')
+        processor_3 = request.form.get('processor_3')
+
+        processor = processor_1 + ' ' + processor_2 + ' ' + processor_3
+
+        mobo_1 = request.form['mobo_1']
+        mobo_2 = request.form['mobo_2']
+        mobo = mobo_1 + ' ' +mobo_2
+
+
+
+
+        ps_1 = request.form['ps_1']
+        ps_2 = request.form['ps_2']
+        ps = ps_1 + ' ' + ps_2
+
+
+
+        ram_1 = request.form['ram_1']
+        ram_2 = request.form.get('ram_2')
+        ram_3 = request.form.get('ram_3')
+        ram = ram_1 + ' ' + ram_2 + ' ' + ram_3
+
         asset_tag = request.form['asset_tag']
         serial_number = request.form['serial_number']
-        storage = request.form.get('storage_1' + ' ' + 'storage_2' + ' ' + 'storage_3')
+
+
+        storage_1 = request.form['storage_1']
+        storage_2 = request.form.get('storage_2')
+        storage_3 = request.form['storage_3']
+        storage = storage_1 + ' ' + storage_2 + ' ' + storage_3
+
+
         os = request.form.get('os_1')
         ms_type = request.form.get('ms_type')
         computer_tag = request.form.get('computer_tag')
         network_tag = request.form.get('network_tag')
-        #eset = request.form['btn_eset']
+        eset = request.form['btn_eset']
         ups = request.form['btn_ups']
         opstat = request.form['btn_opstat']
 
@@ -140,12 +170,13 @@ def add_inventory2():
         #                "RAM = ? STORAGE = ? OS = ? MS_OFFICE = ? ASSET_TAG = ? COMPUTER_TAG = ? NETWORK_TAG = ? UPS = ? SERIAL_NUMBER = ? OPERATIONAL_STATUS = ? ", (department, user, computer_name, ip, processor, mobo, ps,  ram, storage, os, ms_type,
         #                 asset_tag, computer_tag, network_tag,  ups, serial_number, opstat))
         cursor.execute(
-            "INSERT INTO INV_computers (DEPARTMENT, COMPUTER_NAME, IP, PROCESSOR, MOTHERBOARD, POWER_SUPPLY, RAM, STORAGE, OS, MS_OFFICE, ASSET_TAG, COMPUTER_TAG, NETWORK_TAG, UPS, SERIAL_NUMBER, OPERATIONAL_STATUS) "
-            "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            (department, computer_name, ip, processor, mobo, ps, ram, storage, os, ms_type, asset_tag,
-             computer_tag, network_tag, ups, serial_number, opstat))
+            "INSERT INTO INV_computers (DEPARTMENT, USERNAME, COMPUTER_NAME, IP, PROCESSOR, MOTHERBOARD, POWER_SUPPLY, RAM, STORAGE, OS, MS_OFFICE, ASSET_TAG, COMPUTER_TAG, NETWORK_TAG, UPS, SERIAL_NUMBER, OPERATIONAL_STATUS, ESET) "
+            "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (department, user, computer_name, ip, processor, mobo, ps, ram, storage, os, ms_type, asset_tag,
+             computer_tag, network_tag, ups, serial_number, opstat, eset))
 
         connect.commit()
+        # flash("Data Saved Successfully")
 
     return render_template('add_inventory2.html')
 
@@ -153,11 +184,15 @@ def add_inventory2():
 def Modified_Details():
 
     return render_template('Modified_Details.html')
+
+
 @app.route('/Report_Logs', methods=['POST', 'GET'])
 def Report_Logs():
 
+    cursor.execute('SELECT * FROM INV_logs')
+    get_page = cursor.fetchall()
 
-    return render_template('Report_Logs.html')
+    return render_template('Report_Logs.html', get_page=get_page)
 
 
 '''
